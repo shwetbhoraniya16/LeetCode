45f86bdf4176c0ed2d0722bbbe4a065d1b68e136
shwetbhoraniya16 <shwetbhoraniya16@gmail.com>
2026-09-26 22:57:58 +0530

    LC-to-lower-case [Runtime: 0 ms; Memory: 43 MB]

--- string/to-lower-case.java
+++ string/to-lower-case.java
@@ -1,5 +1,12 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        char[] chars = s.toCharArray();
+        for(int i=0; i<chars.length; i++){
+            char ch = chars[i];
+            if(ch >= 'A' && ch <= 'Z'){
+                 chars[i] = (char)(ch + 32);
+            }
+        }
+        return new String(chars);
     }
 }
