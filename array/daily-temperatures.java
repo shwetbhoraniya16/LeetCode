9fcf42cfef3d2c24079a2f9baef90774ebe74b6e
shwetbhoraniya16 <shwetbhoraniya16@gmail.com>
2026-09-26 21:05:37 +0530

    LC-daily-temperatures [Runtime: 77 ms; Memory: 106.6 MB]

---
+++ array/daily-temperatures.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] t) {
+        Stack<Integer> s = new Stack<>();
+        int n = t.length;
+        int[] ans = new int[n];
+
+        for(int i = n-1; i>=0; i--){
+            while(!s.isEmpty() && t[s.peek()] <= t[i]){
+                s.pop();
+            }
+            if(!s.isEmpty()){
+                ans[i] = s.peek() - i;
+            }
+            s.push(i);
+        }
+        return ans;
+    }
+}
